35422b5cc7466bd769c57f635baab5fbc19c6fe0
Owen Grace <owen@homerlearning.com>
2023-02-11 16:11:18 -0500

    tinkering with some oddio preloading in Splash scene

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -1,47 +1,53 @@
 <script>
+try {
+	if (typeof navigator === "undefined") throw "skip server-side execution";
+
 	let Phaser;
 	let Splash;
 	let GameScene;
-	let readyToLaunchPhaser = false;
-	if (typeof navigator !== "undefined") {
-		const phaserPromise = import("phaser").then(imported => {
-			//console.log(`** phaser imported. obj keys:`, Object.keys(imported));
-			Phaser = imported;
-		});
-		const splashPromise = import("../scenes/Splash").then(imported => {
-			console.log(`** Splash imported. obj keys:`, Object.keys(imported));
-			Splash = imported.default;
-		});
-		const gameScenePromise = import("../scenes/GameScene").then(imported => {
-			GameScene = imported.default;
-		});
-		Promise.all([phaserPromise, splashPromise, gameScenePromise]).then(() => {
-			readyToLaunchPhaser = true;
+	//let readyToLaunchPhaser = false;
 
-			new Phaser.Game({
-				type: Phaser.AUTO,
-				parent: 'gamearea',
-				//backgroundColor: '#33A5E7',
-				scale: {
-					width: 1365,
-					height: 768,
-					mode: Phaser.Scale.FIT,
-					autoCenter: Phaser.Scale.CENTER_BOTH
-				},
-				physics: {
-					default: "arcade",
-					arcade: {
-						// debug: true,
-					},
-				},
-				audio: {
-					disableWebAudio: false,
-				},
-				scene: [Splash, GameScene]
-			});
+	const phaserPromise = import("phaser").then(imported => {
+		//console.log(`** phaser imported. obj keys:`, Object.keys(imported));
+		Phaser = imported;
+	});
+	const splashPromise = import("../scenes/Splash").then(imported => {
+		//console.log(`** Splash imported. obj keys:`, Object.keys(imported));
+		Splash = imported.default;
+	});
+	const gameScenePromise = import("../scenes/GameScene").then(imported => {
+		GameScene = imported.default;
+	});
+	Promise.all([phaserPromise, splashPromise, gameScenePromise]).then(() => {
+		//readyToLaunchPhaser = true;
 
+		new Phaser.Game({
+			type: Phaser.AUTO,
+			parent: 'gamearea',
+			//backgroundColor: '#33A5E7',
+			scale: {
+				width: 1365,
+				height: 768,
+				mode: Phaser.Scale.FIT,
+				autoCenter: Phaser.Scale.CENTER_BOTH
+			},
+			physics: {
+				default: "arcade",
+				arcade: {
+					// debug: true,
+				},
+			},
+			audio: {
+				disableWebAudio: false,
+			},
+			scene: [Splash, GameScene]
 		});
-	}
+
+	});
+
+} catch (err) {
+	console.warn(`Game.svelte:`, err.message || err);
+}
 </script>
 
 
--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -1,4 +1,6 @@
 import Phaser from 'phaser';
+import Oddio from '$lib/Oddio';
+import { delay } from '$lib/utils'
 
 export default class Splash extends Phaser.Scene {
 	constructor() {
@@ -11,13 +13,100 @@ export default class Splash extends Phaser.Scene {
 	}
 
 	preload() {
+		console.log(`preload()`);
 		this.load.image('bg', 'assets/outerSpaceBG.jpg');
 		this.load.image('playButtonUp', 'assets/PlayButton-up.png');
 		this.load.image('playButtonDown', 'assets/PlayButton-down.png');
 		this.load.audio('clickUp', 'assets/buttonClick.mp3');
+		this.preloadOddio();
 	}
 
-	create() {
+	async preloadOddio() {
+		console.log(`preloadOddio()`);
+		try {
+			//loadSoundsButton.innerText = "Loading Sound Set Sources";
+			//loadSoundsButton.disabled = true;
+
+			//const soundSet = activeProjDocs.soundSets?.data();
+			//const configJson = JSON.parse(soundSet.configJson);
+			
+			const configJson = {
+				"stereoTrackz": {
+					"__type": "soundSet",
+					"__version": "1.0",
+					"ideas": "cut up the clips above to start where they need to? i should make the playback engine well where i can start any piece from any point, it backtracks events and figures out what is playing and with what offset and volume and pan etc. asset loading should be all about managing whats in mem, with a load and unload event that fires a little in advance of its needs.",
+					"sounds": {
+						"drums": "https://www.openears.net/jog/02_drums.ogg",
+						"metal": "https://www.openears.net/jog/02_metal.ogg"
+					}
+				},
+				"monoTrackz": {
+					"__type": "soundSet",
+					"__version": "1.0",
+					"sounds": {
+						"acoustic1": "https://www.openears.net/jog/02_acoustic_1.ogg",
+						"acoustic2": "https://www.openears.net/jog/02_acoustic_2.ogg",
+						"acoustic3": "https://www.openears.net/jog/02_acoustic_3.ogg",
+						"bass": "https://www.openears.net/jog/02_bass.ogg",
+						"ebow1": "https://www.openears.net/jog/02_ebow_1.ogg",
+						"ebow2": "https://www.openears.net/jog/02_ebow_2.ogg",
+						"ebow3": "https://www.openears.net/jog/02_ebow_3.ogg",
+						"ebow4": "https://www.openears.net/jog/02_ebow_4.ogg",
+						"electricGuitar": "https://www.openears.net/jog/02_electric_guitar.ogg",
+						"odd1": "https://www.openears.net/jog/02_odd_1.ogg",
+						"odd2": "https://www.openears.net/jog/02_odd_2.ogg",
+						"odd3": "https://www.openears.net/jog/02_odd_3.ogg",
+						"synth": "https://www.openears.net/jog/02_synth.ogg"
+					}
+				}
+			};
+
+			const soundSetKeys = Object.keys(configJson);
+			const allLoadPromises = soundSetKeys.map(async ssKey => {
+				const ssSounds = configJson[ssKey].sounds;
+				const soundKeys = Object.keys(ssSounds);
+				//console.log(`- configJson.sounds (keys):`, soundKeys);
+
+				const ssLoadPromises = soundKeys.map(async sKey => {
+					// each value can be a filename string, or an array of filenames
+					const soundFilenames = Array.isArray(ssSounds[sKey]) ? ssSounds[sKey] : [ssSounds[sKey]];
+					const buffs = soundFilenames.map(filename => Oddio.setBuff(filename, {
+						stateCallback: (state) => {
+							console.log(`... load state for ${state.filename} = ${state.stateData}`);
+							/*
+							sBuffs.update(obj => {
+								obj[state.filename] = state.stateData;
+								return obj;
+							});
+							*/
+						}
+					}));
+					Oddio.setSound(sKey, buffs);
+
+					const loadPromise = await Oddio.load(soundFilenames);
+					return loadPromise;
+				});
+				const ssGroupPromise = await Promise.all(ssLoadPromises);
+				console.log(`preloadOddio "${ssKey}" sounds are all ready to go!`);
+				return ssGroupPromise;
+			});
+
+			// add a 3s delay
+			allLoadPromises.push(delay(1000));
+
+			await Promise.all(allLoadPromises);
+			
+			//loadSoundsButton.innerText = "Load Sound Set Sources";
+			//loadSoundsButton.disabled = false;
+			console.log(`preloadOddio(): DONE`);
+		} catch (err) {
+			console.error(`preloadOddio() ERROR:`, err);
+		}
+
+	
+	}
+
+	async create() {
 		//this.cameras.main.setBackgroundColor('#421278');
 
 		this.clickAudio = this.sound.add('clickUp');
@@ -35,9 +124,8 @@ export default class Splash extends Phaser.Scene {
 		this.buttonContainer.add([this.buttonDownSprite, this.buttonUpSprite]);
 
 		this.buttonContainer.scale = 0.5;
+		this.buttonContainer.setAlpha(0.5);
 
-		const hitArea = new Phaser.Geom.Circle(0, 0, this.buttonUpSprite.displayWidth * 0.5);
-		this.buttonContainer.setInteractive(hitArea, Phaser.Geom.Circle.Contains);
 		//this.buttonContainer.on('pointerover', this.onButtonOver, this);
 		//this.buttonContainer.on('pointerout', this.onButtonOut, this);
 		this.buttonContainer.on('pointerdown', this.onButtonDown, this);
@@ -55,6 +143,17 @@ export default class Splash extends Phaser.Scene {
 				this.onButtonUp(e);
 			}
 		});
+
+		// turn off interactivity
+		this.buttonContainer.disableInteractive();
+
+		// preload oddio, when done, 
+		await this.preloadOddio();
+
+		// setInteractive and make full alpha
+		const hitArea = new Phaser.Geom.Circle(0, 0, this.buttonUpSprite.displayWidth * 0.5);
+		this.buttonContainer.setInteractive(hitArea, Phaser.Geom.Circle.Contains);
+		this.buttonContainer.setAlpha(1);
 	}
 
 	onButtonOver(e) {
